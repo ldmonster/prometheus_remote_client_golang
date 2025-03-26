fe84d8e72c9f451c528e0e4b6878e88aed0c2e60
Paul <evilcorefriends@gmail.com>
2025-03-26 07:44:48 +0300

    histo first try

--- cmd/promremotecli/main.go
+++ cmd/promremotecli/main.go
@@ -278,42 +278,153 @@ func MetricFamiliesToTimeSeries(
 				})
 			}
 
-			// Add metric name as a label
-			labels = append(labels, promremote.Label{
-				Name:  "__name__",
-				Value: metricName,
-			})
-
-			value := 0.0
+			// Store timestamp for consistent use across series
+			timestamp := time.Unix(0, metric.GetTimestampMs()*int64(time.Millisecond))
 
 			// Extract value based on metric type
 			switch {
 			case metric.GetCounter() != nil:
-				value = metric.GetCounter().GetValue()
-			case metric.GetGauge() != nil:
-				value = metric.GetGauge().GetValue()
-			case metric.GetHistogram() != nil:
-				value = metric.GetHistogram().GetSampleSum()
-				for _, v := range metric.GetHistogram().GetBucket() {
-					fmt.Println(v.String())
-				}
-				// You might want to handle histogram buckets separately here
-			case metric.GetSummary() != nil:
-				value = metric.GetSummary().GetSampleSum()
-				for _, v := range metric.GetSummary().GetQuantile() {
-					fmt.Println(v.String())
-				}
-				// You might want to handle summary quantiles separately here
-			}
+				// Add counter as a single time series
+				counterLabels := append(labels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName,
+				})
 
-			// Create and add the time series
-			series = append(series, promremote.TimeSeries{
-				Labels: labels,
-				Datapoint: promremote.Datapoint{
-					Timestamp: time.Unix(0, metric.GetTimestampMs()*int64(time.Millisecond)),
-					Value:     value,
-				},
-			})
+				series = append(series, promremote.TimeSeries{
+					Labels: counterLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     metric.GetCounter().GetValue(),
+					},
+				})
+
+			case metric.GetGauge() != nil:
+				// Add gauge as a single time series
+				gaugeLabels := append(labels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName,
+				})
+
+				series = append(series, promremote.TimeSeries{
+					Labels: gaugeLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     metric.GetGauge().GetValue(),
+					},
+				})
+
+			case metric.GetHistogram() != nil:
+				histogram := metric.GetHistogram()
+
+				// 1. Add sum time series
+				sumLabels := append([]promremote.Label{}, labels...)
+				sumLabels = append(sumLabels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName + "_sum",
+				})
+
+				series = append(series, promremote.TimeSeries{
+					Labels: sumLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     histogram.GetSampleSum(),
+					},
+				})
+
+				// 2. Add count time series
+				countLabels := append([]promremote.Label{}, labels...)
+				countLabels = append(countLabels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName + "_count",
+				})
+
+				series = append(series, promremote.TimeSeries{
+					Labels: countLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     float64(histogram.GetSampleCount()),
+					},
+				})
+
+				// 3. Add bucket time series
+				for _, bucket := range histogram.GetBucket() {
+					bucketLabels := append([]promremote.Label{}, labels...)
+					bucketLabels = append(bucketLabels,
+						promremote.Label{
+							Name:  "le",
+							Value: fmt.Sprintf("%g", bucket.GetUpperBound()),
+						},
+						promremote.Label{
+							Name:  "__name__",
+							Value: metricName + "_bucket",
+						},
+					)
+
+					series = append(series, promremote.TimeSeries{
+						Labels: bucketLabels,
+						Datapoint: promremote.Datapoint{
+							Timestamp: timestamp,
+							Value:     float64(bucket.GetCumulativeCount()),
+						},
+					})
+				}
+
+			case metric.GetSummary() != nil:
+				summary := metric.GetSummary()
+
+				// 1. Add sum time series
+				sumLabels := append([]promremote.Label{}, labels...)
+				sumLabels = append(sumLabels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName + "_sum",
+				})
+
+				series = append(series, promremote.TimeSeries{
+					Labels: sumLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     summary.GetSampleSum(),
+					},
+				})
+
+				// 2. Add count time series
+				countLabels := append([]promremote.Label{}, labels...)
+				countLabels = append(countLabels, promremote.Label{
+					Name:  "__name__",
+					Value: metricName + "_count",
+				})
+
+				series = append(series, promremote.TimeSeries{
+					Labels: countLabels,
+					Datapoint: promremote.Datapoint{
+						Timestamp: timestamp,
+						Value:     float64(summary.GetSampleCount()),
+					},
+				})
+
+				// 3. Add quantile time series
+				for _, quantile := range summary.GetQuantile() {
+					quantileLabels := append([]promremote.Label{}, labels...)
+					quantileLabels = append(quantileLabels,
+						promremote.Label{
+							Name:  "quantile",
+							Value: fmt.Sprintf("%g", quantile.GetQuantile()),
+						},
+						promremote.Label{
+							Name:  "__name__",
+							Value: metricName,
+						},
+					)
+
+					series = append(series, promremote.TimeSeries{
+						Labels: quantileLabels,
+						Datapoint: promremote.Datapoint{
+							Timestamp: timestamp,
+							Value:     quantile.GetValue(),
+						},
+					})
+				}
+			}
 		}
 
 		result[metricName] = series
